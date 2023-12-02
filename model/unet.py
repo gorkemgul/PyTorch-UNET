9214d5106308fbe85daccc470e6384ecd928567c
Görkem Gül <104680758+gorkemgul@users.noreply.github.com>
2023-12-02 21:25:51 +0300

    update: implement rest of the model implementation

--- model/unet.py
+++ model/unet.py
@@ -17,22 +17,6 @@ class ConvBnReluBlock(nn.Module):
     def forward(self, x):
         return self.convbnrelu(x)
 
-class BottleNeck(nn.Module):
-    def __init__(self, in_channels = 512, out_channels = 1024, bn_act = False):
-        super().__init__()
-        self.bottleneck = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size = 3, stride = 1, padding = 1, bias = not bn_act),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace = True),
-            nn.Conv2d(out_channels, out_channels, kernel_size = 3, stride = 1, padding = 1, bias = not bn_act),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace = True)
-        )
-
-    def forward(self, x):
-        return self.bottleneck(x)
-
-
 class UNET(nn.Module):
     def __init__(self, in_channels = 3, out_channels = 1):
         super().__init__()
@@ -47,10 +31,27 @@ class UNET(nn.Module):
             in_channels = feature
 
         for feature in self.features_up:
-            self.up.append(nn.ConvTranspose2d(feature, feature // 2, kernel_size = 3, stride = 2))
+            self.up.append(nn.ConvTranspose2d(feature, feature // 2, kernel_size = 2, stride = 2))
             self.up.append(ConvBnReluBlock(feature, feature // 2))
 
-        self.bottleneck = BottleNeck()
+        self.bottleneck = ConvBnReluBlock(in_channels = 512, out_channels = 1024)
         self.last_conv = nn.Conv2d(in_channels = 64, out_channels = out_channels, kernel_size = 1)
 
-        # TODO: rest will be implemented
+    def forward(self, x):
+        skip_cons = []
+
+        for _down in self.down:
+            x = _down(x)
+            skip_cons.append(x)
+            x = self.maxpool(x)
+
+        x = self.bottleneck(x)
+        skip_cons = skip_cons[::-1]
+
+        for idx in range(0, len(self.up), 2):
+            x = self.up[idx](x)
+            skip_con = skip_cons[idx // 2]
+            concat_skip_con = torch.cat((skip_con, x), dim = 1)
+            x = self.up[idx + 1](concat_skip_con)
+
+        return self.last_conv(x)
